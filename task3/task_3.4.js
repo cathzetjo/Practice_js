0e14ff39b8f919bb4fbe9fdf44543eec2e01c73d
Katerina Zhadko <katerinkaj@icloud.com>
2019-04-08 03:41:29 +0300

    style: First commit

---
+++ task3/task_3.4.js
@@ -0,0 +1,9 @@
+/*
+Cars
+Now we will create a car assembly shop.
+It is necessary to make a chain of functions
+that will add new properties-details to the object of the future car.
+Need to:
+- transfer object to each of function,
+- add property,
+- return the original object with new property.*/
